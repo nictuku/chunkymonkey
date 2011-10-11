44294005fb5bc4cb698c8635d815909f380f0963
John Beisley <greatred@gmail.com>
2011-10-11 21:49:35 +0100

    pktHandler now uses proto2.

--- src/chunkymonkey/connhandler.go
+++ src/chunkymonkey/connhandler.go
@@ -12,7 +12,6 @@ import (
 	"chunkymonkey/proto"
 	"chunkymonkey/server_auth"
 	"chunkymonkey/shardserver"
-	. "chunkymonkey/types"
 	"chunkymonkey/worldstore"
 	"nbt"
 )
@@ -112,9 +111,7 @@ func (ch *ConnHandler) run() {
 type pktHandler struct {
 	gameInfo *GameInfo
 	conn     net.Conn
-
-	connType int
-	username string
+	ps       proto.PacketSerializer
 }
 
 func (l *pktHandler) handle() {
@@ -126,38 +123,38 @@ func (l *pktHandler) handle() {
 			if clientErr == nil {
 				clientErr = clientErrGeneral
 			}
-			proto.WriteDisconnect(l.conn, clientErr.String())
+			l.ps.WritePacket(l.conn, &proto.PacketDisconnect{
+				Reason: clientErr.String(),
+			})
 			l.conn.Close()
 		}
 	}()
 
-	err = proto.ServerReadPacketExpect(l.conn, l, []byte{
-		proto.PacketIdHandshake,
-		proto.PacketIdServerListPing,
-	})
+	pkt, err := l.ps.ReadPacketExpect(l.conn, true, 0x02, 0xfe)
 	if err != nil {
 		clientErr = clientErrLoginGeneral
 		return
 	}
 
-	switch l.connType {
-	case connTypeLogin:
-		err, clientErr = l.handleLogin(l.conn)
-	case connTypeServerQuery:
-		err, clientErr = l.handleServerQuery(l.conn)
+	switch p := pkt.(type) {
+	case *proto.PacketHandshake:
+		err, clientErr = l.handleLogin(p)
+	case *proto.PacketServerListPing:
+		err, clientErr = l.handleServerQuery()
 	default:
 		err = loginErrorConnType
 	}
 }
 
-func (l *pktHandler) handleLogin(conn net.Conn) (err, clientErr os.Error) {
-	if !validPlayerUsername.MatchString(l.username) {
+func (l *pktHandler) handleLogin(pktHandshake *proto.PacketHandshake) (err, clientErr os.Error) {
+	username := pktHandshake.UsernameOrHash
+	if !validPlayerUsername.MatchString(username) {
 		err = clientErrUsername
 		clientErr = err
 		return
 	}
 
-	log.Print("Client ", conn.RemoteAddr(), " connected as ", l.username)
+	log.Print("Client ", l.conn.RemoteAddr(), " connected as ", username)
 
 	// TODO Allow admins to connect.
 	if l.gameInfo.maintenanceMsg != "" {
@@ -167,21 +164,21 @@ func (l *pktHandler) handleLogin(conn net.Conn) (err, clientErr os.Error) {
 	}
 
 	// Load player permissions.
-	permissions := gamerules.Permissions.UserPermissions(l.username)
+	permissions := gamerules.Permissions.UserPermissions(username)
 	if !permissions.Has("login") {
-		err = fmt.Errorf("Player %q does not have login permission", l.username)
+		err = fmt.Errorf("Player %q does not have login permission", username)
 		clientErr = clientErrLoginDenied
 		return
 	}
 
-	if err = proto.ServerWriteHandshake(conn, l.gameInfo.serverId); err != nil {
+	if err = l.ps.WritePacket(l.conn, &proto.PacketHandshake{l.gameInfo.serverId}); err != nil {
 		clientErr = clientErrHandshake
 		return
 	}
 
 	if l.gameInfo.serverId != "-" {
 		var authenticated bool
-		authenticated, err = l.gameInfo.authserver.Authenticate(l.gameInfo.serverId, l.username)
+		authenticated, err = l.gameInfo.authserver.Authenticate(l.gameInfo.serverId, username)
 		if !authenticated || err != nil {
 			var reason string
 			if err != nil {
@@ -189,17 +186,14 @@ func (l *pktHandler) handleLogin(conn net.Conn) (err, clientErr os.Error) {
 			} else {
 				reason = "Failed authentication"
 			}
-			err = fmt.Errorf("Client %v: %s", conn.RemoteAddr(), reason)
+			err = fmt.Errorf("Client %v: %s", l.conn.RemoteAddr(), reason)
 			clientErr = clientErrAuthFailed
 			return
 		}
-		log.Print("Client ", conn.RemoteAddr(), " passed minecraft.net authentication")
+		log.Print("Client ", l.conn.RemoteAddr(), " passed minecraft.net authentication")
 	}
 
-	err = proto.ServerReadPacketExpect(conn, l, []byte{
-		proto.PacketIdLogin,
-	})
-	if err != nil {
+	if _, err = l.ps.ReadPacketExpect(l.conn, true, 0x01); err != nil {
 		clientErr = clientErrLoginGeneral
 		return
 	}
@@ -207,18 +201,18 @@ func (l *pktHandler) handleLogin(conn net.Conn) (err, clientErr os.Error) {
 	entityId := l.gameInfo.entityManager.NewEntity()
 
 	var playerData *nbt.Compound
-	if playerData, err = l.gameInfo.game.worldStore.PlayerData(l.username); err != nil {
+	if playerData, err = l.gameInfo.game.worldStore.PlayerData(username); err != nil {
 		clientErr = clientErrUserData
 		return
 	}
 
-	player := player.NewPlayer(entityId, l.gameInfo.shardManager, conn, l.username, l.gameInfo.worldStore.SpawnPosition, l.gameInfo.game.playerDisconnect, l.gameInfo.game)
+	player := player.NewPlayer(entityId, l.gameInfo.shardManager, l.conn, username, l.gameInfo.worldStore.SpawnPosition, l.gameInfo.game.playerDisconnect, l.gameInfo.game)
 	if playerData != nil {
 		if err = player.UnmarshalNbt(playerData); err != nil {
 			// Don't let the player log in, as they will only have default inventory
 			// etc., which could lose items from them. Better for an administrator to
 			// sort this out.
-			err = fmt.Errorf("Error parsing player data for %q: %v", l.username, err)
+			err = fmt.Errorf("Error parsing player data for %q: %v", username, err)
 			clientErr = clientErrUserData
 			return
 		}
@@ -230,7 +224,7 @@ func (l *pktHandler) handleLogin(conn net.Conn) (err, clientErr os.Error) {
 	return
 }
 
-func (l *pktHandler) handleServerQuery(conn net.Conn) (err, clientErr os.Error) {
+func (l *pktHandler) handleServerQuery() (err, clientErr os.Error) {
 	err = loginErrorServerList
 	clientErr = fmt.Errorf(
 		"%s§%d§%d",
@@ -238,57 +232,3 @@ func (l *pktHandler) handleServerQuery(conn net.Conn) (err, clientErr os.Error) 
 		l.gameInfo.game.PlayerCount(), l.gameInfo.maxPlayerCount)
 	return
 }
-
-func (l *pktHandler) PacketServerLogin(username string) {
-}
-
-func (l *pktHandler) PacketServerHandshake(username string) {
-	l.connType = connTypeLogin
-	l.username = username
-}
-
-func (l *pktHandler) PacketServerListPing() {
-	l.connType = connTypeServerQuery
-}
-
-func (l *pktHandler) PacketKeepAlive(id int32) {}
-
-func (l *pktHandler) PacketChatMessage(message string) {}
-
-func (l *pktHandler) PacketPlayer(onGround bool) {}
-
-func (l *pktHandler) PacketHoldingChange(slotId SlotId) {}
-
-func (l *pktHandler) PacketEntityAction(entityId EntityId, action EntityAction) {}
-
-func (l *pktHandler) PacketUseEntity(user EntityId, target EntityId, leftClick bool) {}
-
-func (l *pktHandler) PacketRespawn(dimension DimensionId, unknown int8, gameType GameType, worldHeight int16, mapSeed RandomSeed) {
-}
-
-func (l *pktHandler) PacketPlayerPosition(position *AbsXyz, stance AbsCoord, onGround bool) {}
-
-func (l *pktHandler) PacketPlayerLook(look *LookDegrees, onGround bool) {}
-
-func (l *pktHandler) PacketPlayerBlockHit(status DigStatus, blockLoc *BlockXyz, face Face) {}
-
-func (l *pktHandler) PacketPlayerBlockInteract(itemTypeId ItemTypeId, blockLoc *BlockXyz, face Face, amount ItemCount, data ItemData) {
-}
-
-func (l *pktHandler) PacketEntityAnimation(entityId EntityId, animation EntityAnimation) {}
-
-func (l *pktHandler) PacketUnknown0x1b(field1, field2 float32, field3, field4 bool, field5, field6 float32) {
-}
-
-func (l *pktHandler) PacketUnknown0x3d(field1, field2 int32, field3 int8, field4, field5 int32) {}
-
-func (l *pktHandler) PacketWindowClose(windowId WindowId) {}
-
-func (l *pktHandler) PacketWindowClick(windowId WindowId, slot SlotId, rightClick bool, txId TxId, shiftClick bool, expectedSlot *proto.WindowSlot) {
-}
-
-func (l *pktHandler) PacketWindowTransaction(windowId WindowId, txId TxId, accepted bool) {}
-
-func (l *pktHandler) PacketSignUpdate(position *BlockXyz, lines [4]string) {}
-
-func (l *pktHandler) PacketDisconnect(reason string) {}
